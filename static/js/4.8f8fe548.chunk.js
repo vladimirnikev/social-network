85313106abb322a5cb937523ed2604d3d79bf1e9
Vladimir <vladimirnikev@gmail.com>
2021-03-17 17:27:17 +0200

    Updates

---
+++ static/js/4.8f8fe548.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-01"]=this["webpackJsonpreact-01"]||[]).push([[4],{298:function(t,e,c){"use strict";c.r(e);var s=c(0);e.default=function(){return Object(s.jsx)("div",{children:"News"})}}}]);
+//# sourceMappingURL=4.8f8fe548.chunk.js.map
